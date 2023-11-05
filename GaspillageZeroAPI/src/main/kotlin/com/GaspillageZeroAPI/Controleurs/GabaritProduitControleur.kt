25f56e18c7bd08c192bb05b61a939626d112b28a
Samuel <1977223@crosemont.qc.ca>
2023-11-05 17:49:46 -0500

    Controller GabaritProduit avec méthodes vides

---
+++ GaspillageZeroAPI/src/main/kotlin/com/GaspillageZeroAPI/Controleurs/GabaritProduitControleur.kt
@@ -0,0 +1,30 @@
+package com.GaspillageZeroAPI.Controleurs
+
+import org.springframework.web.bind.annotation.*
+
+@RestController
+class GabaritProduitController {
+
+    // MÉTHODES
+
+    // MÉTHODE AFFICHAGES
+    //@GetMapping("/gabaritproduits")
+    //fun obtenirGabaritProduits() = ...
+
+    // MÉTHODE AFFICHAGES 2
+    //@GetMapping("/gabaritproduit/{code}")
+    //fun obtenirGabaritProduitParCode(@PathVariable code: String) = ...
+
+    // MÉTHODE AJOUTER
+    //@PostMapping("/gabaritproduits")
+    //fun Ajouter(@RequestBody gabaritproduit :Gabaritproduit) = ...
+
+    // MÉTHODE MODIFICATION
+    //@PutMapping("/gabaritproduit/{code}")
+    //fun ModifyParCode(@PathVariable code: String, @RequestBody gabaritproduit: Gabaritproduit) = ...
+
+    // MÉTHODE SUPPRIMER
+    //@DeleteMapping("/gabaritproduit/{code}")
+    //fun DeleteParCode(@PathVariable code: String) = ...
+
+}
