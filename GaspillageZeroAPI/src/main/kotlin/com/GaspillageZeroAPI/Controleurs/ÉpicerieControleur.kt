a7873dc02cdc4031e521ce938b1786f197935fea
Samuel <1977223@crosemont.qc.ca>
2023-11-05 18:57:49 -0500

    Amélioration Commentaires ÉpicerieControleur + Mis en commentaire des Méthodes ÉpicerieControleur(Ajouter, Modifier, Supprimer)

--- GaspillageZeroAPI/src/main/kotlin/com/GaspillageZeroAPI/Controleurs/ÉpicerieControleur.kt
+++ GaspillageZeroAPI/src/main/kotlin/com/GaspillageZeroAPI/Controleurs/ÉpicerieControleur.kt
@@ -7,24 +7,28 @@ import org.springframework.web.bind.annotation.*
 @RestController
 class ÉpicerieControleur(val service: ÉpicerieService) {
 
+    // MÉTHODES
+
+    // MÉTHODE AFFICHAGES - LISTE ÉPICERIES
     @GetMapping("/épiceries")
     fun obtenirÉpiceries() = service.chercherTous()
 
+    // MÉTHODE AFFICHAGES 2 - ÉPICERIE PAR CODE
     @GetMapping("/épicerie/{idÉpicerie}")
     fun obtenirÉpicerieparCode(@PathVariable idÉpicerie: Int) = service.chercherParCode(idÉpicerie)
 
-    @PostMapping("/épicerie")
-    fun ajouterÉpicerie(@RequestBody épicerie: Épicerie) {
-        service.ajouter(épicerie)
-    }
+    //@PostMapping("/épicerie")
+    //fun ajouterÉpicerie(@RequestBody épicerie: Épicerie) {
+    //    service.ajouter(épicerie)
+    //}
 
-    @DeleteMapping("/épicerie/delete/{idÉpicerie}")
-    fun suppimerÉpicerie(@PathVariable idÉpicerie: Int) {
-        service.supprimer(idÉpicerie)
-    }
+    //@DeleteMapping("/épicerie/delete/{idÉpicerie}")
+    //fun suppimerÉpicerie(@PathVariable idÉpicerie: Int) {
+    //    service.supprimer(idÉpicerie)
+    //}
 
-    @PutMapping("/épicerie/save")
-    fun modifierÉpicerie(@PathVariable idÉpicerie: Int, @RequestBody épicerie: Épicerie){
-        service.modifier(idÉpicerie, épicerie)
-    }
+    //@PutMapping("/épicerie/save")
+    //fun modifierÉpicerie(@PathVariable idÉpicerie: Int, @RequestBody épicerie: Épicerie){
+    //    service.modifier(idÉpicerie, épicerie)
+    //}
 }
