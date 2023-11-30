d49ccd5ae9efa0c7b9c626bf7e5ea77c848ff95f
Alikhan Khakimov <akhakimov@dept-info.crosemont.quebec>
2023-11-29 20:41:12 -0500

    UtilisateurDAOImpl

--- GaspillageZeroAPI/src/main/kotlin/com/GaspillageZeroAPI/DAO/UtilisateurDAOImpl.kt
+++ GaspillageZeroAPI/src/main/kotlin/com/GaspillageZeroAPI/DAO/UtilisateurDAOImpl.kt
@@ -1,6 +1,8 @@
 package com.GaspillageZeroAPI.DAO
 
+import com.GaspillageZeroAPI.Exceptions.ExceptionErreurServeur
 import com.GaspillageZeroAPI.Exceptions.ExceptionRessourceIntrouvable
+import com.GaspillageZeroAPI.Modèle.Produit
 import com.GaspillageZeroAPI.Modèle.Utilisateur
 import com.GaspillageZeroAPI.Modèle.UtilisateursTable
 import org.springframework.jdbc.core.JdbcTemplate
@@ -10,32 +12,63 @@ import java.sql.ResultSet
 @Repository
 class UtilisateurDAOImpl(private val jdbcTemplate: JdbcTemplate): UtilisateurDAO {
 
-    override fun chercherTous(): List<Utilisateur> = SourceDonnées.utilisateurs
-    //override fun chercherParCode(idUtilisateur: Int): Utilisateur? = SourceDonnées.utilisateurs.find{it.idUtilisateur == idUtilisateur}
-    override fun chercherParCode(id: Int): Utilisateur? {
-        return jdbcTemplate.queryForObject("SELECT * from Utilisateur WHERE code = ?", arrayOf(id)) { resultat, _ ->
+    override fun chercherTous(): List<Utilisateur> {
+        return jdbcTemplate.query("SELECT * FROM utilisateur") { resultat, _ ->
             mapRowToUtilisateur(resultat)
+        }
+    }
+    override fun chercherParCode(id: Int): Utilisateur? {
+        var utilisateur: Utilisateur? = null
+
+        try {
+            utilisateur = jdbcTemplate.queryForObject<Utilisateur>("SELECT * FROM utilisateur WHERE code=?", arrayOf(id)){ resultat, _ ->
+                mapRowToUtilisateur(resultat)
+            }
+        }catch (e: Exception){}
+
+        return utilisateur
+    }
+
+    private fun obtenirProchaineIncrementationIDUtilisateur():Int?{
+        return jdbcTemplate.queryForObject(
+                "SELECT auto_increment FROM INFORMATION_SCHEMA.TABLES WHERE table_schema = 'gaspillagealimentaire' AND table_name = 'utilisateur'"
+        ) { resultat, _ ->
+            resultat.getInt("auto_increment")
         }
     }
 
     override fun ajouter(utilisateur: Utilisateur): Utilisateur? {
-        SourceDonnées.utilisateurs.add(utilisateur)
-        return utilisateur
+        val id = obtenirProchaineIncrementationIDUtilisateur()
+        try{
+            jdbcTemplate.update("INSERT INTO utilisateur(nom, prénom, courriel, adresse_id, téléphone, rôle) VALUES (?,?,?,?,?,?)",
+                    utilisateur.nom, utilisateur.prénom, utilisateur.courriel, utilisateur.adresse?.idAdresse, utilisateur.téléphone, utilisateur.rôle)
+        }catch(e: java.lang.Exception){throw e}
+        SourceDonnées.produits.add(utilisateur)
+        if(id!=null) {
+            return chercherParCode(id)
+        }else {
+            return null
+        }
     }
 
     override fun supprimer(idUtilisateur: Int): Utilisateur? {
-        val utilisateurSuppimer = SourceDonnées.utilisateurs.find { it.code == idUtilisateur }
-        if (utilisateurSuppimer != null){
-            SourceDonnées.utilisateurs.remove(utilisateurSuppimer)
-        }else{
+        if(chercherParCode(idUtilisateur)==null){
             throw ExceptionRessourceIntrouvable("L'utilisateur avec le ID $idUtilisateur est introuvable")
         }
-        return utilisateurSuppimer
+        try{
+            jdbcTemplate.update("DELETE FROM utilisateur WHERE code=?", idUtilisateur)
+        } catch (e:Exception){
+            throw ExceptionErreurServeur("erreur: " + e.message)
+        }
+
+        return null
     }
 
     override fun modifier(idUtilisateur: Int, utilisateur: Utilisateur): Utilisateur? {
-        val indexModifierUtilisateur = SourceDonnées.utilisateurs.indexOf(SourceDonnées.utilisateurs.find { it.code == idUtilisateur })
-        SourceDonnées.utilisateurs.set(indexModifierUtilisateur, utilisateur)
+        try{
+            jdbcTemplate.update("UPDATE utilisateur SET nom=?, prénom=?, courriel=?, adresse_id=?, téléphone=?, rôle=? WHERE code=?",
+                    utilisateur.nom, utilisateur.prénom, utilisateur.courriel, utilisateur.adresse?.idAdresse, utilisateur.téléphone, utilisateur.rôle, idUtilisateur)
+        }catch (e:Exception){throw e}
         return utilisateur
     }
     private fun mapRowToUtilisateur(rs: ResultSet): Utilisateur {
