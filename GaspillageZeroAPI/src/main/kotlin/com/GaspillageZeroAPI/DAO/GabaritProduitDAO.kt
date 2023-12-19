73188fba8862d8fd5ed284c939fa1f2c662d09c5
Samuel <1977223@crosemont.qc.ca>
2023-12-19 02:36:50 -0500

    GabaritProduit : Authentification/Autorisation - Vérification si Connecté et role épicerie

--- GaspillageZeroAPI/src/main/kotlin/com/GaspillageZeroAPI/DAO/GabaritProduitDAO.kt
+++ GaspillageZeroAPI/src/main/kotlin/com/GaspillageZeroAPI/DAO/GabaritProduitDAO.kt
@@ -9,4 +9,5 @@ interface GabaritProduitDAO : DAO<GabaritProduit> {
     override fun ajouter(gabaritProduit:GabaritProduit): GabaritProduit?
     override fun supprimer(id: Int): GabaritProduit?
     override fun modifier(id: Int, gabaritProduit: GabaritProduit): GabaritProduit?
+    fun estGerantParCode(code: String): Boolean
 }
