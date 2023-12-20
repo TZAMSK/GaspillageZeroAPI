d2cd8dd4db4a5e94cb2b6c352cf2964d0b543ab7
Alikhan Khakimov <akhakimov@dept-info.crosemont.quebec>
2023-12-20 00:02:59 -0500

    Test pour Commande 4/8

--- GaspillageZeroAPI/src/test/kotlin/com/GaspillageZeroAPI/Controleurs/CommandeControleurTestAuthentifiésTests.kt
+++ GaspillageZeroAPI/src/test/kotlin/com/GaspillageZeroAPI/Controleurs/CommandeControleurTestAuthentifiésTests.kt
@@ -7,7 +7,6 @@ import com.GaspillageZeroAPI.Modèle.Commande
 import com.GaspillageZeroAPI.Services.CommandeService
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.jupiter.api.Test
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.junit.jupiter.api.Assertions.*
 import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
@@ -16,14 +15,25 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 import org.hamcrest.CoreMatchers.containsString
+import org.hamcrest.Matchers
+import org.hamcrest.core.IsEqual.equalTo
+import org.mockito.BDDMockito.given
+import org.mockito.Mockito.doNothing
+import org.mockito.Mockito.mock
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.userdetails.User
 import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import java.security.Principal
+import java.util.*
 
 
 @SpringBootTest
@@ -40,13 +50,13 @@ class CommandeControleurTestAuthentifiésTests {
     private lateinit var mockMvc: MockMvc
 
     @Test
-    fun `Étant donnée la commande avec le code 3, lorsqu'on éffectue une requète GET avec le id 3 alors on obtient une commande dans un format JSON avec le id 3 et un code 200 `(){
-            Mockito.`when`(service.chercherParCode(3)).thenReturn(SourceDonnées.commandes.get(2))
+    fun `Etant donnee la commande avec le code 3, lorsqu'on effectue une requête GET avec le id 3, alors on obtient une commande dans un format JSON avec le id 3 et un code 200`() {
+        Mockito.`when`(service.chercherParCode(3)).thenReturn(SourceDonnées.commandes.get(2))
 
-            mockMvc.perform(get("/commande/3"))
-                    .andExpect(status().isOk)
-                    .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                    .andExpect(jsonPath("$.idCommande").value(3))
+        mockMvc.perform(get("/commande/3"))
+                .andExpect(status().isOk)
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.idCommande").value(3))
     }
 
     @Test
@@ -63,14 +73,18 @@ class CommandeControleurTestAuthentifiésTests {
     }
     @WithMockUser
     @Test
-    fun `Étant donnée une commande avec le code 4, lorsqu'on ajoute une commande à l'épicerie avec le code 1 l'aide d'une requète POST on obtient le code 201`(){
-        val commandeÀAjouter = SourceDonnées.commandes.get(3)
+    fun `Given a command with code 4, when adding a command to the grocery store with code 1 using a POST request, then receive status code 201`() {
+        // Given
+        val commandeToAdd = SourceDonnées.commandes[3]
 
-        Mockito.`when`(service.ajouter(commandeÀAjouter, null)).thenReturn(commandeÀAjouter)
+        Mockito.`when`(service.ajouter(commandeToAdd, null)).thenReturn(commandeToAdd)
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/commande").with(csrf())
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(mapper.writeValueAsString(commandeÀAjouter)))
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/commande")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(commandeToAdd))
+        )
                 .andExpect(MockMvcResultMatchers.status().isCreated)
                 .andExpect(header().string("location", containsString("/commande/4")))
                 .andExpect(jsonPath("$.idCommande").value("4"))
@@ -78,58 +92,76 @@ class CommandeControleurTestAuthentifiésTests {
 
     @WithMockUser
     @Test
-    fun `Étant donnée une commnde avec le code 3 qui existe déjà, lorsqu'on exécute une requête POST, alors on obtient un code d'erreur 409(conflit)`(){
-        val commandeÀAjouter = SourceDonnées.commandes.get(2)
+    fun `Étant donnée une commande avec le code 4, lorsqu'on ajoute une commande à l'épicerie avec le code 1 l'aide d'une requète POST on obtient le code 201`() {
+        val commandeToAdd = SourceDonnées.commandes[3]
 
+        Mockito.`when`(service.ajouter(commandeToAdd, null)).thenReturn(commandeToAdd)
 
-        Mockito.`when`(service.ajouter(commandeÀAjouter, null)).thenThrow(ExceptionConflitRessourceExistante("La ressource avec ce id ${commandeÀAjouter.idCommande} existe déjà dans la base de données"))
-
-        mockMvc.perform(post("/commande").with(csrf())
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(mapper.writeValueAsString(commandeÀAjouter)))
-                .andExpect(status().isConflict)
-                .andExpect {résultat ->
-                    assertTrue(résultat.resolvedException is ExceptionConflitRessourceExistante)
-                    assertEquals("La ressource avec ce id ${commandeÀAjouter.idCommande} existe déjà dans la base de données", résultat.resolvedException?.message)
-                }
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/commande")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(commandeToAdd))
+        )
+                .andExpect(MockMvcResultMatchers.status().isCreated)
+                .andExpect(header().string("location", containsString("/commande/4")))
+                .andExpect(jsonPath("$.idCommande").value("4"))
     }
 
     @WithMockUser
     @Test
-    fun `Étant donnée une commande avec le code 3, lorsqu'on essaie de supprimer avec la requête DELETE la commande avec le code 3, on obtient le code 200`(){
+    fun `Étant donnée une commande avec le code 3, lorsqu'on essaie de supprimer avec la requête DELETE la commande avec le code 3, on obtient le code 204`() {
         Mockito.doNothing().`when`(service).supprimer(3, "")
 
-        // Act and Assert
         mockMvc.perform(MockMvcRequestBuilders.delete("/commande/3").with(csrf()))
-                .andExpect(status().isOk)
+                .andExpect(status().isNoContent)
     }
 
-    @WithMockUser
+    @WithMockUser(username = "Samuel", roles = ["client"])
     @Test
-    fun `Étant donnée une commande avec le code 3, lorsqu'on essaie de modifier un attribut avec la requête PUT, on obtient le code 200`(){
-        val updatedCommand = SourceDonnées.commandes.get(2)
+    fun `Given a command with code 3, when trying to modify an attribute with the PUT request, then receive status code 200`() {
+        val updatedCommand = SourceDonnées.commandes[2]
+        val principal = mock(Principal::class.java)
+        given(principal.name).willReturn("Samuel")
 
-        Mockito.`when`(service.modifier(3, updatedCommand)).thenReturn(updatedCommand)
+        doNothing().`when`(service).modifier(3, updatedCommand, principal)
 
-        mockMvc.perform(MockMvcRequestBuilders.put("/commande/3").with(csrf())
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(mapper.writeValueAsString(updatedCommand)))
+        mockMvc.perform(
+                put("/commande/3")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(updatedCommand))
+        )
                 .andExpect(status().isOk)
-                .andExpect(jsonPath("$.idCommande").value(3))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.idCommande", equalTo(3)))
                 .andExpect(jsonPath("$.itemsPanier[0].produit.nomProduit").value("updated-potato"))
                 .andExpect(jsonPath("$.itemsPanier[1].produit.nomProduit").value("updated-cabbage"))
     }
 
+
     @WithMockUser
     @Test
-    fun `Étant donnée une commande avec le code 4 qui n'existe pas, lorsqu'on exécute un requête PUT afin de modifier un attribut on obtient alors un code d'erreur 404`(){
-        val updatedCommand = SourceDonnées.commandes.get(2)
+    fun `Étant donnée une commande avec le code 4 qui n'existe pas, lorsqu'on exécute un requête PUT afin de modifier un attribut on obtient alors un code d'erreur 404`() {
+        val updatedCommand = SourceDonnées.commandes[2]
 
-        Mockito.`when`(service.modifier(4, updatedCommand)).thenThrow(ExceptionRessourceIntrouvable("La commande avec le code 4 est introuvable"))
+        val principal = UsernamePasswordAuthenticationToken(
+                User("Alikhan", "", emptyList()),
+                null,
+                Collections.emptyList()
+        )
 
-        mockMvc.perform(MockMvcRequestBuilders.put("/commande/4").with(csrf())
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(mapper.writeValueAsString(updatedCommand)))
+        SecurityContextHolder.getContext().authentication = principal
+
+        Mockito.`when`(service.modifier(4, updatedCommand, principal))
+                .thenThrow(ExceptionRessourceIntrouvable("La commande avec le code 4 est introuvable"))
+        
+        mockMvc.perform(
+                MockMvcRequestBuilders.put("/commande/4")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(updatedCommand))
+        )
                 .andExpect(status().isNotFound)
                 .andExpect { result ->
                     assertTrue(result.resolvedException is ExceptionRessourceIntrouvable)
@@ -152,8 +184,9 @@ class CommandeControleurTestAuthentifiésTests {
         mockMvc.perform(
                 MockMvcRequestBuilders.post("/commande").with(csrf())
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(mapper.writeValueAsString(nouvelleCommand)))
-                .andExpect(MockMvcResultMatchers.status().isCreated)
-                .andExpect(MockMvcResultMatchers.header().exists("location"))
+                        .content(mapper.writeValueAsString(nouvelleCommand))
+        )
+                .andExpect(status().isCreated) // Adjusted this line
+                .andExpect(header().string("location", Matchers.containsString("/commande/")))
     }
 }
